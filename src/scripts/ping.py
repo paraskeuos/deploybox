9f4e20ca3c4d2fc4cb38334c222b8bfc7230ffc9
paraskeuos <57093460+paraskeuos@users.noreply.github.com>
2023-12-20 18:12:12 +0100

    Add fullstops to output

--- src/scripts/ping.py
+++ src/scripts/ping.py
@@ -3,12 +3,12 @@ from lib.hostutils import load_hosts, remote_command, get_key_path
 
 if __name__ == "__main__":
 	if len(sys.argv) != 2:
-		sys.stderr.write("Incorrect usage\n")
+		sys.stderr.write("Incorrect usage.\n")
 		exit(1)
 	
 	hosts = load_hosts()
 	if sys.argv[1] not in hosts:
-		sys.stderr.write(f"Could not find host with name '{sys.argv[1]}'\n")
+		sys.stderr.write(f"Could not find host with name '{sys.argv[1]}'.\n")
 		exit(1)
 
 	host = hosts[sys.argv[1]]
