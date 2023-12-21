444f589b97377827ff9a445dee32099f95dc4494
paraskeuos <57093460+paraskeuos@users.noreply.github.com>
2023-12-21 19:06:00 +0100

    Add deletion of key and node info

--- src/scripts/unregister_node.py
+++ src/scripts/unregister_node.py
@@ -1,5 +1,5 @@
 import sys
-from lib.hostutils import load_hosts, save_hosts
+from lib.hostutils import load_hosts, save_hosts, delete_key
 
 if __name__ == "__main__":
 	if len(sys.argv) != 2:
@@ -10,3 +10,9 @@ if __name__ == "__main__":
 	if sys.argv[1] not in hosts.keys():
 		sys.stderr.write(f"Could not find node with name '{sys.argv[1]}'.\n")
 		exit(1)
+
+	hosts.pop(sys.argv[1])
+	delete_key(sys.argv[1])
+	save_hosts(hosts)
+
+	sys.stdout.write(f"Node '{sys.argv[1]}' unregistered successfully.\n")
